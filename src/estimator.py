58e536389ca84be008dc68111fd250b6c2da5b26
Faith Oyedemi <faithoyedemi@yahoo.com>
2020-04-13 09:14:51 +0100

    ...

--- src/estimator.py
+++ src/estimator.py
@@ -37,7 +37,7 @@ def estimator(data):
 
     # estimate available number of hospital beds for severe cases
     # expect that maximum of 35% of total bede spaces will be available
-    available = math.floor(data['totalHospitalBeds'] * 0.35)
+    available = math.ceil(data['totalHospitalBeds'] * 0.35)
 
     if impact['severeCasesByRequestedTime'] <= available:
         impact['hospitalBedsByRequestedTime'] = available
